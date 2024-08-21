05e6fab8b8c304ee891c9f8451b5028001431d2e
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2024-08-21 20:45:52 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Taffezinho";
+
+        /*
+        *  Operadores Logicos
+        *  >  Maior
+        *  >= Maior igual
+        *  <  Menor
+        *  <= Menor igual
+        *  == é igual
+        *  != é diferente
+        *  equal compara Textos
+        * */
+
+        if(idadePessoa > 21){
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
+
+
     }
 }
